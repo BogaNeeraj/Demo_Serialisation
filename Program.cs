9f3673703b15ffcc3dbee67cbc76614ae1aaeb66
BogaNeeraj <92246820+BogaNeeraj@users.noreply.github.com>
2021-10-18 18:44:24 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Demo_Serialisation
+{
+    public class WeatherForecast
+
+    {
+        public DateTimeOffset Date { get; set; }
+
+        public int TemperatureCelsius { get; set; }
+
+        public string Summary { get; set; }
+
+    }
+    public class program
+    {
+
+        static void Main(string[] args)
+        {
+            var WeatherForecast = new WeatherForecast
+
+            {
+                Date = DateTime.Parse("2021-09-01"),
+                TemperatureCelsius = 29,
+                Summary = " Delhi Weather is Extreme either too Much Hot or Cold"
+
+            };
+
+            string weatherString = JsonSerializer.Serialize(WeatherForecast);
+            Console.WriteLine("My Json string is {0}", weatherString);
+        }
+    }
+}
+
+    
